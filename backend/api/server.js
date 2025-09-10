d31a978c37ad82fd1d85a424d2ec2479e3a8a9d3
anujpatel002 <swaminarayan2181@gmail.com>
2025-09-10 19:13:26 +0530

    6th try

--- backend/api/server.js
+++ backend/api/server.js
@@ -101,5 +101,4 @@ app.use('/api', healthRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5001;
-httpServer.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+export default app;
